2df31b00816392483f4404f6655a4d0d46b9f39d
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-08-05 10:33:32 +0700

    update code

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -322,7 +322,7 @@
                                             <ul class="dropdown-menu">
                                                 <li><a href="/xe-nang-dien">Xe nâng điện</a>
                                                 </li>
-                                                <li><a href="/xe-nang-dau">Xe nâng dầu 12234</a>
+                                                <li><a href="/xe-nang-dau">Xe nâng dầu</a>
                                                 </li>
                                                 <li><a href="/xe-nang-tay">Xe nâng tay</a>
                                                 </li>
