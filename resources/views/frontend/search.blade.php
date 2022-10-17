adc5d36f5fa9016cd47ba68c06ca1270f29f0fd9
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2022-10-17 16:17:52 +0700

    update

---
+++ resources/views/frontend/search.blade.php
@@ -0,0 +1,40 @@
+@extends('frontend.layouts.apps')
+@section('content') 
+<section id="productshop">
+    <div class="container">
+        <div class="title-info wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">
+            <div class="slogan">
+                Chọn các sản phẩm nổi bật từ chúng tôi. Có thể bạn sẽ thấy thích chúng          
+            </div>
+        </div>
+        <div class="tab-content">
+            <div id="tab0" class="tab-pane fade in active">
+
+                @if($datas->count()>0)
+               
+                @foreach($datas as $val)
+               
+                <div class="col-xs-6 col-sm-6 col-md-3">
+                    <div class="infobox wow bounceIn">
+                        <div class="img-box">
+                            <a href="{{ route('product-details', $val->link) }}">
+                            <img src="{{ asset($val->images) }}" class="img-responsive">
+                            </a>
+                        </div>
+                        <a href="{{ route('product-details', $val->link) }}" class="title-box">{{ $val->name }}</a>
+                        <div class="price-box">
+                            {{ @str_replace(',' ,'.', number_format($val->price)) }}đ                       
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+
+                @endif
+              
+            </div>
+        </div>
+       
+    </div>
+    
+</section>
+@endsection
